dfc1dff49548d15c2c8f3d4124144c3d478b5a86
SGSouthard <sydneyaileensouthard@gmail.com>
2021-11-08 14:37:17 -0800

    app.js: Added button variables

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//Establishing Menu Button Variables
+let howToButton = document.querySelector("#how-to");
+let playGameButton = document.querySelectorAll(".play-game");
+let mainMenuButton = document.querySelectorAll(".main-menu");
+let quitButton = document.querySelector("#quit");
+
+//Establishing Play Buttons
+let shotsButton = document.querySelector("#shots");
+let flavorButton = document.querySelector("#flavor");
+let milkButton = document.querySelector("#milk");
+let sugarButton = document.querySelector("#sugar");
+let resetButton = document.querySelector("#reset");
+let serveButton = document.querySelector("#serve");
+
+//Switching Screens
+
+//The Timer
+
+//The Tip Counter
+
+//The game Interval
+
+//
